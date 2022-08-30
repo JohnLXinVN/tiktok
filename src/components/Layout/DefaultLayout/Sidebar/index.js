0aa139a4004628e6e9cf4c65c2cc323798e633ee
anhhm-john <anhhm.2003.work@gmail.com>
2022-08-30 13:44:37 +0700

    Layout header

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,10 +1,16 @@
 
 
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss'
+
+const cx = classNames.bind(styles)
 
 
 function Sidebar() {
     return (
-        <h1>Sidebar</h1>
+        <div className={cx('wrapper')}>
+            <h1>Sidebar</h1>
+        </div>
     );
 }
 
